aafba1ad3811ae8cba13af95a5826fc99effd834
Andrey S <134010168+Innstenar@users.noreply.github.com>
2023-09-27 00:55:07 +0300

    Update test_add_to_favorites.py
    Added a check for the presence of an ad on the page before attempting to add it to favorites.

--- qa_tasks/test_add_to_favorites.py
+++ qa_tasks/test_add_to_favorites.py
@@ -10,25 +10,30 @@ def test_add_to_favorites():
 
         page.goto(advertisement_url)  # Переход на страницу объявления
         
-        # Получение названия товара
-        item_name = page.locator("span.title-info-title-text").inner_text()
+        # Проверяем наличие элемента с заголовком объявления
+        if page.locator("span.title-info-title-text").is_visible():
+        
+            # Получение названия товара
+            item_name = page.locator("span.title-info-title-text").inner_text()
 
-        # Нажатие на кнопку "Добавить в избранное"
-        add_to_favorite_button = page.locator("div.style-header-add-favorite-M7nA2>button.desktop-usq1f1")
-        add_to_favorite_button.click()
+            # Нажатие на кнопку "Добавить в избранное"
+            add_to_favorite_button = page.locator("div.style-header-add-favorite-M7nA2>button.desktop-usq1f1")
+            add_to_favorite_button.click()
 
-        # Ожидание, пока кнопка перейдет в состояние "Добавлено в избранное"
-        page.wait_for_selector("div[class=style-header-add-favorite-M7nA2]>button[data-is-favorite=true]")
+            # Ожидание, пока кнопка перейдет в состояние "Добавлено в избранное"
+            page.wait_for_selector("div[class=style-header-add-favorite-M7nA2]>button[data-is-favorite=true]")
 
-        # Переход в избранное
-        go_to_favorites_button = page.locator("div.desktop-1rdftp2")
-        go_to_favorites_button.click()
+            # Переход в избранное
+            go_to_favorites_button = page.locator("div.desktop-1rdftp2")
+            go_to_favorites_button.click()
 
-        # Ожидание загрузки страницы избранного и проверка наличия добавленного товара
-        page.wait_for_selector(".styles-module-root-hwVld")
-        assert item_name in page.content(), "Товар не добавился в избранное"
-        print("Объявление успешно добавлено в избранное")
-
+            # Ожидание загрузки страницы избранного и проверка наличия добавленного товара
+            page.wait_for_selector(".styles-module-root-hwVld")
+            assert item_name in page.content(), "Товар не добавился в избранное"
+            print("Объявление успешно добавлено в избранное")
+        else:
+            print("Объявление было снято с публикации")
+            
 # Запуск теста
 if __name__ == "__main__":
     test_add_to_favorites()
